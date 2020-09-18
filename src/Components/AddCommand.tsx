158d146183d98733887351a29b7a4a755a013ca9
Heda <hotheda@gmail.com>
2020-09-18 22:42:40 +0200

    add post works

--- src/Components/AddCommand.tsx
+++ src/Components/AddCommand.tsx
@@ -1,23 +1,73 @@
 import React, { useState } from "react"
 
+interface dbData {
+    name: string,
+    platform: string,
+    description: string,
+    options: string,
+    howTo:  string
+};
+
+
 const AddCommand:React.FC = () => {
     const [addNew, setAddNew] = useState<boolean>(false)
+    
+    const [myData, setMyData] = useState<dbData>({name:"",platform:"",description:"",options:"",howTo:""});
+    
+    const postData = ()=>{
+        fetch('http://192.168.1.153:5555/addpost', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(myData)
+        })
+        .then(res => { return res.json() })
+        .then(data => console.log(data))
+        .catch(err => console.log("Error: ", err))
+    }
 
-    const addToDB = () => {
+    const addToDB = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.preventDefault();
         //check for all fields to be filled
-        //post jSon object to db
+        console.log("Json som skickas:" + JSON.stringify(myData))
+        postData();
+        setAddNew(!addNew);
+    }
+
+    const onTextChange = (e:React.ChangeEvent<HTMLInputElement>)=> {
+        var newData:dbData = myData;
+        switch(e.target.id){
+            case"name":
+                newData.name=e.target.value;
+                return;
+            case"platform":
+                newData.platform=e.target.value;
+                return;
+            case"description":
+                newData.description=e.target.value;
+                return;
+            case"options":
+                newData.options=e.target.value;
+                return;
+            case"howTo":
+                newData.howTo=e.target.value;
+                return;
+        }
+        setMyData(newData);
     }
     
     if(addNew)
     return(
         <div>
-            <h3>Add new item</h3>
-            <label>Name: </label><input placeholder="name"/><br/>
-            <label>Platform: </label><input placeholder="Platform"/><br/>
-            <label>Description: </label><input placeholder="Description"/><br/>
-            <label>Options: </label><input placeholder="Options"/><br/>
-            <label>Example: </label><input placeholder="Example"/><br/>
-            <button onClick={()=>{setAddNew(!addNew)}} >Add command</button>
+            <form>
+                <h3>Add new item</h3>
+                <label>Name: </label><input onChange={(e)=>onTextChange(e)} id="name" placeholder="name" type="Text" required/><br/>
+                <label>Platform: </label><input onChange={(e)=>onTextChange(e)} id="platform" placeholder="Platform" type="Text" /><br/>
+                <label>Description: </label><input onChange={(e)=>onTextChange(e)} id="description" placeholder="Description" type="Text" /><br/>
+                <label>Options: </label><input onChange={(e)=>onTextChange(e)} id="options" placeholder="Options" type="Text" /><br/>
+                <label>Example: </label><input onChange={(e)=>onTextChange(e)} id="howTo" placeholder="Example" type="Text" /><br/>
+                <button onClick={(e)=>{addToDB(e)}} >Add command</button>
+                <button onClick={()=>{setAddNew(!addNew)}} >Cancel</button>
+            </form>
         </div>
     )
 
--- src/Components/Command.tsx
+++ src/Components/Command.tsx
@@ -1,43 +1,14 @@
 import React, { useEffect, useState } from "react";
 
-/*interface item {
-    id:number,
-    name:string,
-    platform:string,
-    description:string,
-    options:string,
-    howTo:string
-}*/
-
 interface Props {
     selectedItem:any;
 }
 
 
 const Command:React.FC<Props> = ({selectedItem}) => {
-    // name: , howTo: , options: , platform: , description: 
-    //const [allData, setAllData] = useState<any>();
     const allData = selectedItem;
-
-    /*
-    useEffect(() => {
-        getData();
-    },[]);
-
-    const id:number = selectedItem;
-
-    const getData = () => {
-        fetch('http://192.168.1.153:5555/getpost/'+id.toString())
-            .then(res => res.json())
-            .then(data => {
-                if(data)
-                    setAllData(data);
-            })
-            .catch(err => console.log("Error ", err))
-    }*/
     
     console.log(selectedItem)
-    //console.log(allData);
 
     if(allData)
     return(
