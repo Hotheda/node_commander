fff2f571f46894b803769634391604e5f37f7205
Heda <hotheda@gmail.com>
2020-11-01 22:29:31 +0100

    Updates

---
+++ precache-manifest.dd13b347e220e06062b3eb1cc3370d73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d94c4477bd5535e333ad9787dc603d8",
+    "url": "/node_commander/index.html"
+  },
+  {
+    "revision": "c92b2c056d9c5b3a2e4b",
+    "url": "/node_commander/static/css/main.79806f29.chunk.css"
+  },
+  {
+    "revision": "a080d886da01d90ff38a",
+    "url": "/node_commander/static/js/2.248ab50d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/node_commander/static/js/2.248ab50d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92b2c056d9c5b3a2e4b",
+    "url": "/node_commander/static/js/main.e37cbd3b.chunk.js"
+  },
+  {
+    "revision": "15977890adf344f10c0c",
+    "url": "/node_commander/static/js/runtime-main.3f3b18f6.js"
+  }
+]);
